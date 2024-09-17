0fa7e12691e084b3023dda2e79e59ea23c512203
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2024-09-17 21:29:26 +0200

    feat: added data fetching to display context

--- contexts/display-context.tsx
+++ contexts/display-context.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useRTDB } from '@/hooks/use-rtdb';
 import { DisplayType } from '@/types/display-types';
+import { trpc } from '@/utils/trpc';
 import { createContext, FC } from 'react';
 
 export interface DisplayContextType {
@@ -15,7 +16,18 @@ export const DisplayProvider: FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   const { data: displayType } = useRTDB<DisplayType>({ path: '/active' });
-
+  const {
+    data: metric,
+    isLoading,
+    error,
+  } = trpc.firebaseAPI.getMetric.useQuery(displayType as string, {
+    enabled: !!displayType,
+  });
+  const { data } = useRTDB({
+    path: `/data/${displayType}`,
+    start: metric?.config.type === 'ANALOG',
+  });
+  console.log(data, isLoading, error);
   const contextValue: DisplayContextType = {
     displayType,
   };
