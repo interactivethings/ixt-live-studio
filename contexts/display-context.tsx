c0c947d8c469677250ca6a689d487f6d81c6971f
Noah Onyejese <129368606+noahonyejese@users.noreply.github.com>
2024-09-18 07:16:56 +0200

    feat:Added data sync & normalization functions

--- contexts/display-context.tsx
+++ contexts/display-context.tsx
@@ -1,35 +1,37 @@
 'use client';
 import { useRTDB } from '@/hooks/use-rtdb';
-import { DisplayType } from '@/types/display-types';
-import { trpc } from '@/utils/trpc';
-import { createContext, FC } from 'react';
+import { MetricTypes } from '@/types/display-types';
+import {
+  getMetricType,
+  MetricBody,
+  normalizeData,
+  NormalizedMetricNode,
+} from '@/utils/normalize';
+import { createContext, FC, useContext } from 'react';
+import { useConfig } from './config-context';
 
-export interface DisplayContextType {
-  displayType: DisplayType | null;
+export interface DisplayContextType<T> {
+  displayData: NormalizedMetricNode<T>[];
 }
 
-export const DisplayContext = createContext<DisplayContextType | undefined>(
-  undefined
-);
+export const DisplayContext = createContext<
+  DisplayContextType<unknown> | undefined
+>(undefined);
 
 export const DisplayProvider: FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const { data: displayType } = useRTDB<DisplayType>({ path: '/active' });
-  const {
-    data: metric,
-    isLoading,
-    error,
-  } = trpc.firebaseAPI.getMetric.useQuery(displayType as string, {
-    enabled: !!displayType,
+  const { displayType } = useConfig();
+
+  const { data } = useRTDB<MetricBody>({
+    path: `/data/` + displayType,
   });
-  const { data } = useRTDB({
-    path: `/data/${displayType}`,
-    start: metric?.config.type === 'ANALOG',
-  });
-  console.log(data, isLoading, error);
-  const contextValue: DisplayContextType = {
-    displayType,
+
+  const metricType = getMetricType(displayType as keyof MetricTypes);
+  const displayData = normalizeData<typeof metricType>(data);
+
+  const contextValue: DisplayContextType<typeof metricType> = {
+    displayData,
   };
 
   return (
@@ -37,4 +39,14 @@ export const DisplayProvider: FC<{ children: React.ReactNode }> = ({
       {children}
     </DisplayContext.Provider>
   );
+};
+
+export const useDisplay = <T,>() => {
+  const ctx = useContext(DisplayContext) as DisplayContextType<T>;
+
+  if (!ctx) {
+    throw new Error('useDisplay must be used within a DisplayProvider');
+  }
+
+  return ctx;
 };
